48d732a1198c8a00f45b31faafc48ed1b1fbfaac
Fred <frederico.almeidamoreira@gmail.com>
2026-02-10 21:41:06 -0300

    criado bucket e adicionado tfstate

--- provider.tf
+++ provider.tf
@@ -5,8 +5,16 @@ terraform {
       version = "~> 5.0"
     }
   }
+  
+  backend "s3" {
+    bucket = "curso-comunidadedevops"
+    key    = "dev/terraform.tfstate"
+    region = "us-east-1"
+  }
 }
+
 
 provider "aws" {
   region = "us-east-1"
 }
+
