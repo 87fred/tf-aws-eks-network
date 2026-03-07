b5d0668d574d9c920de0fd93d72e2de1753ea1d6
Fred <frederico.almeidamoreira@gmail.com>
2026-03-07 01:14:21 -0300

    feat: ajustando anotacoes - public.tf

--- public.tf
+++ public.tf
@@ -30,8 +30,8 @@ resource "aws_subnet" "eks_subnet_public_1b" {
 
 #Apontamento/Associação da route table para subnet
 resource "aws_route_table_association" "eks_rtb_assoc_1a" {
-  subnet_id      = aws_subnet.eks_subnet_public_1a.id
-  route_table_id = aws_route_table.eks_public_route_table.id
+  subnet_id      = aws_subnet.eks_subnet_public_1a.id #a subnet a ser apontada/associada
+  route_table_id = aws_route_table.eks_public_route_table.id #rtb a ser apontada/associada par a subnet
 }
 
 resource "aws_route_table_association" "eks_rtb_assoc_1b" {
